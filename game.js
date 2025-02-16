051fce4071c013f7719e44c7c462e16a92843b0d
KhushiMangla <khushimangla99@gmail.com>
2025-02-16 14:21:48 -0800

    applying btn logic

---
+++ game.js
@@ -0,0 +1,37 @@
+
+let boxes= document.querySelectorAll(".box");
+let resetBtn= document.querySelector("#reset-btn")
+
+// Player X & Player Y
+
+let turnO ="true";
+
+let winPatterns= [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+]
+
+for (let box of boxes){
+    box.addEventListener("click", ()=>{
+        console.log("box has clicked");
+       if (turnO){
+        box.innerText= "X";
+        turnO= false;
+       }
+       else{
+        box.innerText= "O";
+        turnO= true;
+       }
+       box.disabled= true;  // btn becomes unclickable;
+    });
+}
+
+
+
+
