555329f8a57bc897afd283426039294e3fbf1ad3
KhushiMangla <khushimangla99@gmail.com>
2025-02-16 23:22:25 -0800

    applied winner logic

--- game.js
+++ game.js
@@ -28,10 +28,28 @@ for (let box of boxes){
         box.innerText= "O";
         turnO= true;
        }
-       box.disabled= true;  // btn becomes unclickable;
+       box.disabled= true; 
+       
+       checkWinner()// btn becomes unclickable;
     });
 }
 
+const checkWinner = ()=>{
+
+    for (let pattern of winPatterns){
+        console.log(pattern[0], pattern[1], pattern[2]);
+         let boxIdx1= boxes[pattern[0]].innerText;
+         let boxIdx2=  boxes[pattern[1]].innerText;
+         let boxIdx3=   boxes[pattern[2]].innerText;
+
+         if (boxIdx1 !="" && boxIdx2 !="" && boxIdx3 !=""){
+            if (boxIdx1=== boxIdx2 && boxIdx2===boxIdx3){
+                console.log("winner is ", boxIdx1);
+           
+            }
+         }
+    }
+}
 
 
 
